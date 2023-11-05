5b3ccfc7ad72afb19e9a6afd2f68ec6a55a621d5
pmihango <pmihango@gmail.com>
2023-11-05 11:33:54 +0300

    added the regidtration form fields

--- registration.php
+++ registration.php
@@ -8,7 +8,15 @@
 <body>
     <div>
         <form action="registration.php" method="post">
-            <div class="container"></div>
+            <div class="container">
+                <h1>Registration</h1>
+                <p>Fill in the forem with correct values</p>
+                <label for="firstname"><b>First Name</b></label> <input type="text" name="firstname" required>
+                <label for="lastname"><b>Last Name</b></label> <input type="text" name="lastname" required>
+                <label for="email"><b>Email Address</b></label> <input type="email" name="email" required>
+                <label for="phonenumber"><b>Phone Number</b></label> <input type="text" name="phonenumber" required>
+                <label for="password"><b>Password</b></label> <input type="password" name="password" required>
+            </div>
         </form>
     </div>
 </body>
