87a445bcfe5ad5c3dfa53033878207094407846e
pmihango <pmihango@gmail.com>
2023-11-05 11:13:42 +0300

    added registration form

---
+++ registration.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>User Registration</title>
+</head>
+<body>
+    <div>
+        <form action="registration.php" method="post">
+            <div class="container"></div>
+        </form>
+    </div>
+</body>
+</html>
